40576bf081bd60eee0ba70de45a7c76bb2d30cb5
S00250496 <S00250496@atu.ie>
2025-12-02 16:19:05 +0000

    Classes Added

---
+++ Pale Roots 1/Mechanics Engines/Healthbar.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprites
+{
+    public class Healthbar
+    {
+        public int health;
+        private Texture2D TxHealthBar; // hold the texture
+        Rectangle healthRect; // display the Health bar size
+        public Vector2 position; // Position on the screen
+
+        public Rectangle 
+            HealthRect { 
+            get => new Rectangle((int)position.X, (int)position.Y, health, 10);
+            set => healthRect = value; }
+
+        public Healthbar(Vector2 Startposition, int healthValue, Game g)
+        {
+            health = healthValue;
+            position = Startposition;
+            TxHealthBar = new Texture2D(g.GraphicsDevice, 1, 1);
+            TxHealthBar.SetData(new[] { Color.White });
+
+        }
+
+        public void Update()
+        {
+            if (health > 0)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Down)) health--;
+            }
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            if (health > 0)
+            {
+                if (health > 60)
+                    spriteBatch.Draw(TxHealthBar, HealthRect, Color.Green);
+                else if (health > 30 && health <= 60)
+                    spriteBatch.Draw(TxHealthBar, HealthRect, Color.Orange);
+                else if (health > 0 && health < 30)
+                    spriteBatch.Draw(TxHealthBar, HealthRect, Color.Red);
+            }
+        }
+    }
+}
---
+++ Pale Roots 1/Player/PlayerWithWeapon.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Engines;
+
+namespace AnimatedSprite
+{
+    
+        class PlayerWithWeapon : Sprite
+        {
+            protected Game myGame;
+            protected float playerVelocity = 6.0f;
+        private Projectile myProjectile;
+        public CrossHair Site;
+        //public SoundEffect shoot;
+        private SoundEffect shoot;
+        //had to add logic for previous keyboard state to prevent multiple firings on a single key press as the shoot sound was constanly playing
+        private KeyboardState previousKeyboardState;
+
+
+        public Vector2 CentrePos
+            {
+                get { return position + new Vector2(spriteWidth/ 2, spriteHeight / 2); }
+                
+            }
+
+        public Projectile MyProjectile
+        {
+            get
+            {
+                return myProjectile;
+            }
+
+            set
+            {
+                myProjectile = value;
+            }
+        }
+
+        public PlayerWithWeapon(Game g, Texture2D texture, Vector2 userPosition, int framecount) : base(g,texture,userPosition,framecount)
+            {
+                myGame = g;
+            var vp = g.GraphicsDevice.Viewport;
+            Site = new CrossHair(g,
+                                 g.Content.Load<Texture2D>("scope2"),
+                                 new Vector2(vp.Width / 2, vp.Height / 2),
+                                 1);
+            shoot = g.Content.Load<SoundEffect>("shoot");
+            //didnt know where to load the asset as there is no load content method here so loaded it in the constructor
+
+        }
+
+        public void loadProjectile(Projectile r)
+            {
+                MyProjectile = r;
+            }
+
+
+        public override void Update(GameTime gameTime)
+        {
+           
+            Viewport gameScreen = myGame.GraphicsDevice.Viewport;
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                this.position += new Vector2(1, 0) * playerVelocity;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                this.position += new Vector2(-1, 0) * playerVelocity;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                this.position += new Vector2(0, -1) * playerVelocity;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                this.position += new Vector2(0, 1) * playerVelocity;
+            }
+            // check for site change
+            
+            Site.Update(gameTime);
+            // Whenever the rocket is still and loaded it follows the player posiion
+            if (MyProjectile != null 
+                && MyProjectile.ProjectileState 
+                == Projectile.PROJECTILE_STATE.STILL)
+                MyProjectile.position = this.CentrePos;
+            // if a roecket is loaded
+            if (MyProjectile != null && MyProjectile.ProjectileState
+                == Projectile.PROJECTILE_STATE.STILL)
+            {
+                KeyboardState ks = Keyboard.GetState();
+                // fire the rocket and it looks for the target
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) && previousKeyboardState.IsKeyUp(Keys.Space))
+                {     //had a probloem here where because the asset i got for the new crosshair was massive, i shrunk the scale but the project was still firing to the top left corner of the origiuonal
+                    //image size not the scaled down size. So i adjusted the target position by half the width and height of the original image to get it to fire to the right place.
+                    //MyProjectile.fire(Site.position);
+                    MyProjectile.fire(Site.position + new Vector2(Site.spriteWidth * 0.1f, Site.spriteHeight * 0.1f));
+                    //shoot = Content.Load<SoundEffect>("shoot");
+                    shoot.Play();
+                }
+
+            }
+
+            // Make sure the player stays in the bounds see previous lab for details
+            position = Vector2.Clamp(position, Vector2.Zero,
+                                            new Vector2(gameScreen.Width - spriteWidth,
+                                                        gameScreen.Height - spriteHeight));
+            
+            // Update the Camera with respect to the players new position
+            //Vector2 delta = cam.Pos - this.position;
+            //cam.Pos += delta;
+            
+            if (MyProjectile != null)
+                MyProjectile.Update(gameTime);
+            // Update the players site
+            //Site.Update(gameTime);
+            // call Sprite Update to get it to animated 
+            base.Update(gameTime);
+        }
+            
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            Site.Draw(spriteBatch);
+            if (MyProjectile != null 
+                && MyProjectile.ProjectileState 
+                != Projectile.PROJECTILE_STATE.STILL)
+                    MyProjectile.Draw(spriteBatch);
+            
+        }
+
+    }
+}
