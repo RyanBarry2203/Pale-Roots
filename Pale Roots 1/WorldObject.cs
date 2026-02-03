ae14d7c8a8c5df04e406250b26ba1f214ddb4da7
S00250496 <S00250496@atu.ie>
2026-02-03 22:36:27 +0000

    added World Objects Class, About to nuke the tile map system

---
+++ Pale Roots 1/WorldObject.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Pale_Roots_1
+{
+    public class WorldObject : Sprite
+    {
+        public bool IsSolid { get; set; }
+
+        public Rectangle CollisionBox
+        {
+            get
+            {
+                int width = (int)(spriteWidth * Scale * 0.6f);
+                int height = (int)(spriteHeight * Scale * 0.3f);
+
+                int x  = (int)Center.X - (width / 2);
+                int y  = (int)(position.Y + (spriteHeight * Scale) - height);
+
+                return new Rectangle(x, y, width, height);
+            }
+        }
+        public WorldObject(Game g, Texture2D texture, Vector2 pos, int frameCount, bool isSolid)
+            : base(g, texture, pos, frameCount, 1.0)
+        {
+            this.IsSolid = isSolid;
+
+            int millisecondsBetweenFrames = 150;
+        }
+        public override void Update(GameTime gametime)
+        {
+            base.Update(gametime);
+        }
+    }
+}
